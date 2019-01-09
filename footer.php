36f731d2acc171f7b4c27af54e37a37b20ca3ab1
Daniil Borovkov <daniilborowkow@gmail.com>
2019-01-09 10:12:19 +0600

    Created initial setup for theme: footer and header

--- footer.php
+++ footer.php
@@ -11,6 +11,26 @@
 
 ?>
 
+<div id="rec47189732" class="r t-rec t-rec_pt_90 t-rec_pb_90">
+    <!-- t564-->
+    <div class="t564">
+        <div class="t-container">
+            <div class="t-col t-col_8 t-prefix_2 t-align_center">
+                <img src="<?php echo get_theme_mod('footer-logo') ?>" alt="" class="t564__img">
+                <div class="t564__small-wrapper">
+                    <div class="t564__text t-text t-text_md">
+                        <div class="t564_text" style="color:#000000;">
+                            <?php echo get_theme_mod( 'phone') ?>
+                            <br>
+                            <a class="t564_email" href="mailto:<?php echo get_theme_mod( 'email') ?>"><?php echo get_theme_mod( 'email') ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </div> <!-- #allrecords -->
 
 <?php wp_footer(); ?>
--- inc/customizer.php
+++ inc/customizer.php
@@ -10,31 +10,78 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function db_smart_light_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+function db_smart_light_customize_register($wp_customize)
+{
+    $wp_customize->get_setting('blogname')->transport         = 'postMessage';
+    $wp_customize->get_setting('blogdescription')->transport  = 'postMessage';
+    $wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
-			'selector'        => '.site-title a',
-			'render_callback' => 'db_smart_light_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-			'selector'        => '.site-description',
-			'render_callback' => 'db_smart_light_customize_partial_blogdescription',
-		) );
-	}
+    if (isset($wp_customize->selective_refresh)) {
+        $wp_customize->selective_refresh->add_partial('blogname', array(
+            'selector'        => '.site-title a',
+            'render_callback' => 'db_smart_light_customize_partial_blogname',
+        ));
+        $wp_customize->selective_refresh->add_partial('blogdescription', array(
+            'selector'        => '.site-description',
+            'render_callback' => 'db_smart_light_customize_partial_blogdescription',
+        ));
+    }
+
+    $wp_customize->add_section('db_smart_light_contacts', array(
+        'title'    => __('Контакты', 'db_smart_light'),
+        'priority' => 30,
+    ));
+
+    $wp_customize->add_setting('phone', array(
+        'default'   => '',
+        'transport' => 'postMessage',
+    ));
+
+    $wp_customize->add_control('phone', array(
+        'label'   => __('Телефон'),
+        'section' => 'db_smart_light_contacts',
+        'type'    => 'text',
+    ));
+
+    $wp_customize->add_setting('email', array(
+        'default'   => '',
+        'transport' => 'postMessage',
+    ));
+
+    $wp_customize->add_control('email', array(
+        'label'   => __('Email'),
+        'section' => 'db_smart_light_contacts',
+        'type'    => 'text',
+    ));
+
+    $wp_customize->add_setting('footer-logo', array(
+        'default'   => '',
+        'transport' => 'postMessage',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control(
+        // $wp_customize object
+        $wp_customize,
+        // $id
+        'footer-logo',
+        // $args
+        array(
+            'section'     => 'db_smart_light_contacts',
+            'label'       => __('Изображение для баннера на главной странице', 'db_smart_light'),
+            'description' => __('Выберите изображение для баннера', 'db_smart_light'),
+        )
+    ));
 }
-add_action( 'customize_register', 'db_smart_light_customize_register' );
+add_action('customize_register', 'db_smart_light_customize_register');
 
 /**
  * Render the site title for the selective refresh partial.
  *
  * @return void
  */
-function db_smart_light_customize_partial_blogname() {
-	bloginfo( 'name' );
+function db_smart_light_customize_partial_blogname()
+{
+    bloginfo('name');
 }
 
 /**
@@ -42,14 +89,16 @@ function db_smart_light_customize_partial_blogname() {
  *
  * @return void
  */
-function db_smart_light_customize_partial_blogdescription() {
-	bloginfo( 'description' );
+function db_smart_light_customize_partial_blogdescription()
+{
+    bloginfo('description');
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function db_smart_light_customize_preview_js() {
-	wp_enqueue_script( 'db-smart-light-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+function db_smart_light_customize_preview_js()
+{
+    wp_enqueue_script('db-smart-light-customizer', get_template_directory_uri() . '/js/customizer.js', array('customize-preview'), '20151215', true);
 }
-add_action( 'customize_preview_init', 'db_smart_light_customize_preview_js' );
+add_action('customize_preview_init', 'db_smart_light_customize_preview_js');
